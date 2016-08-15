b5734790ce30a5d0b7d2317f24ee2a2d8f553ec5
Jeseekia Vaughn <jeseekia@gmail.com>
2016-08-15 19:37:00 -0400

    Modify the get request to request data from /api/todos

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -3,13 +3,13 @@
 angular.module('todoListApp')
 .service('dataService', function($http) {
   this.getTodos = function(cb) {
-    $http.get('/mock/todos.json').then(cb);
+    $http.get('/api/todos').then(cb);
   };
-  
+
   this.deleteTodo = function(todo) {
     console.log("I deleted the " + todo.name + " todo!");
   };
-  
+
   this.saveTodos = function(todos) {
     console.log("I saved " + todos.length + " todos!");
   };
