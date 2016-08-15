02bce1117238bbf97c5e90617bb58c1671cead6e
Jeseekia Vaughn <jeseekia@gmail.com>
2016-08-15 19:12:46 -0400

    Get mock todos, and update /todos route to send the mock todos

--- src/api/index.js
+++ src/api/index.js
@@ -6,13 +6,16 @@ var express = require("express");
 //Router to prefix api/ to routes
 var router =  express.Router();
 
+//Get mock todos
+var todosData = require('../../mock/todos.json');
+
 //API Routes:
 // app.get('/api/todos',function(req,res){
 //   // res.send({todos:[]});
 //   res.json({todos:[]});
 // });
 router.get('/todos',function(req,res){
-  res.json({todos:[]});
+  res.json({todos: todosData});
 });
 
 //TODO: POST route to create new todos
