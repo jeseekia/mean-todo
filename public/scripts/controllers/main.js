040c0c5d8c4f894035a6f90419772e06751f881c
Jeseekia Vaughn <jeseekia@gmail.com>
2016-08-15 19:33:03 -0400

    Modify the todos res so that it works w/the todos api structure.

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -2,15 +2,15 @@
 
 angular.module('todoListApp')
 .controller('mainCtrl', function($scope, dataService){
-  
+
   dataService.getTodos(function(response){
-    var todos = response.data;  
+    var todos = response.data.todos;  
     $scope.todos =  todos;
     });
-  
+
   $scope.addTodo = function() {
     $scope.todos.unshift({name: "This is a new todo.",
                       completed: false});
   };
-  
+
 })
