2b41f5be44bdcb01f26ee22ef95ff05fce4c56ed
Jeseekia Vaughn <jeseekia@gmail.com>
2016-08-15 19:05:36 -0400

    Import api router, and configure router to prefix api/

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var express = require("express");
+//import api router
+var router = require("./api");
+
 
 //Create an instance of the express server for middleware
 //to configure routes and start the server
@@ -8,6 +11,10 @@ var app = express();
 
 //Configure express to serve static files from the public folder
 app.use("/",express.static("public"));
+
+//Prefix /api to routes
+app.use('/api',router);
+
 
 //Configure app to listen for requests on port 3000
 app.listen(3000, function(){
