1a29e14fd1071043eb813eb1517dcd09a78220e7
Jeseekia Vaughn <jeseekia@gmail.com>
2016-08-15 20:10:37 -0400

    Require scripts from angular app for bundling with webpack

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
 'use strict';
 
+var angular = require("angular");
+
 angular.module('todoListApp', []);
+
+require("./scripts/controllers/main.js");
+require("./scripts/controllers/todo.js");
+require("./scripts/directives/todo.js");
+require("./scripts/services/data.js");
